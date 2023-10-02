797bfcf5b31820bf90b7b9eb9fc161a634115ad8
Nadzeya Savitskaya <>
2023-10-02 17:08:09 +0300

    init project

---
+++ SunshineForecast/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  SunshineForecast
+//
+//  Created by Nadzeya Savitskaya on 2023-10-02.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ SunshineForecast/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SunshineForecast
+//
+//  Created by Nadzeya Savitskaya on 2023-10-02.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
